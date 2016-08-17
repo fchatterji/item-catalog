f72d0678c8e6f610beee5288586a5f7c59f1f387
fishesandchip <florent.chatterji@gmail.com>
2016-08-17 22:11:51 +0200

    feat: display one item, one category, can change item name

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,45 @@
 import { Component } from '@angular/core';
+
+export class Item {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export class Category {
+  id: number;
+  name: string;
+}
+
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My Second Angular 2 App</h1>'
+  template:`
+  <h1>{{title}}</h1>
+
+  <h2>{{item.name}} details!</h2>
+  <div><label>id: </label>{{item.id}}</div>
+  <input [(ngModel)]="item.name" placeholder="name">
+  <div><label>description: </label>{{item.description}}</div>
+
+  <h2>{{category.name}} details!</h2>
+  <div><label>id: </label>{{category.id}}</div>
+  <div><label>name: </label>{{category.name}}</div>
+  `
 })
-export class AppComponent { }
+
+export class AppComponent {
+
+  title = 'Item catalog';
+
+  item: Item = {
+  id: 1,
+  name: 'Windstorm',
+  description: 'a storm of wind'
+  };
+
+  category: Category = {
+  id: 1,
+  name: 'tools'
+  };
+}
